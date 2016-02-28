4221077951242975ee4c5822bd801633564e9482
Yaroslav Shukharev <krkmetal@gmail.com>
2016-02-29 01:18:03 +1000

    small parser fix

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ def application(environ, start_response):
     results = ""
     status_code = '200 OK'
     response_headers = [('Content-type','text/plain')]
-    parameters = parse_qs(environ["QUERY_STRING"])
+    parameters = parse_qs(environ["QUERY_STRING"], keep_blank_values=True)
     for parameter in parameters:
         parameter_data = "".join(parameters[parameter])
         results += "{}={}\n".format(parameter, parameter_data)
