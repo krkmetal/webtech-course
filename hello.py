0dde7bc2b37d2e431649c735a68467a5a5cb8b76
Yaroslav Shukharev <krkmetal@gmail.com>
2016-02-29 01:52:01 +1000

    final fix

--- hello.py
+++ hello.py
@@ -1,13 +1,6 @@
-from urlparse import parse_qs
-
 def application(environ, start_response):
-    results = ""
     status_code = '200 OK'
     response_headers = [('Content-type','text/plain')]
-    #parameters = parse_qs(environ["QUERY_STRING"], keep_blank_values=True)
-    parameters = environ["QUERY_STRING"].replace('&','\n')
-    for parameter in parameters:
-        parameter_data = "".join(parameters[parameter])
-        results += "{}={}\n".format(parameter, parameter_data)
+    results = environ["QUERY_STRING"].replace('&','\n')
     start_response(status_code, response_headers)
     return [results]
