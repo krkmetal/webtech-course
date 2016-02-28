6a93d82752d59f236db783639dc8843ccb4adb03
Yaroslav Shukharev <krkmetal@gmail.com>
2016-02-29 00:55:05 +1000

    hello app with gunicorn config 2

---
+++ hello.py
@@ -0,0 +1,12 @@
+from urlparse import parse_qs
+
+def application(environ, start_response):
+    results = ""
+    status_code = '200 OK'
+    response_headers = [('Content-type','text/plain')]
+    parameters = parse_qs(environ["QUERY_STRING"])
+    for parameter in parameters:
+        parameter_data = "".join(parameters[parameter])
+        results += "{}={}\n".format(parameter, parameter_data)
+    start_response(status_code, response_headers)
+    return [results]
