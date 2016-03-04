cc33866410823190258319b976da1cb17a09478d
Yaroslav Shukharev <krkmetal@gmail.com>
2016-03-04 17:09:04 +1000

    change all to 200 response test views

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -7,12 +7,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.test, name='home'),
-    url(r'^login/', 'django.views.defaults.page_not_found', name='login'),
-    url(r'^signup/', 'django.views.defaults.page_not_found', name='signup'),
+    url(r'^login/', views.test, name='login'),
+    url(r'^signup/', views.test, name='signup'),
     url(r'^question/(?P<qid>\w+)/$', views.test),
-    url(r'^ask/', 'django.views.defaults.page_not_found', name='ask'),
-    url(r'^popular/', 'django.views.defaults.page_not_found', name='popular'),
-    url(r'^new/', 'django.views.defaults.page_not_found'),
+    url(r'^ask/', views.test, name='ask'),
+    url(r'^popular/', views.test, name='popular'),
+    url(r'^new/', views.test),
 
     #url(r'^admin/', include(admin.site.urls)),
 )
