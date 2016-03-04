f37f0d880720f944b5a153cd08792f7a87401c64
Yaroslav Shukharev <krkmetal@gmail.com>
2016-03-04 16:57:30 +1000

    add django base

---
+++ ask/ask/settings.py
@@ -0,0 +1,48 @@
+import os
+BASE_DIR = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(BASE_DIR)
+BASE_DIR = os.path.dirname(BASE_DIR)
+
+
+SECRET_KEY = 'whl!8d0-9wzh4fbp$rwnp)w8vpwy7oo-tyj!ls$e&p)o8vji@h'
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'qa'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'ask.urls'
+WSGI_APPLICATION = 'ask.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = BASE_DIR + '/public/'
---
+++ ask/ask/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from qa import views
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'django.views.defaults.page_not_found', name='home'),
+    url(r'^login/', 'django.views.defaults.page_not_found', name='login'),
+    url(r'^signup/', 'django.views.defaults.page_not_found', name='signup'),
+    url(r'^question/(?P<qid>\w+)/$', views.test),
+    url(r'^ask/', 'django.views.defaults.page_not_found', name='ask'),
+    url(r'^popular/', 'django.views.defaults.page_not_found', name='popular'),
+    url(r'^new/', 'django.views.defaults.page_not_found'),
+
+    #url(r'^admin/', include(admin.site.urls)),
+)
