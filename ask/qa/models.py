8e11684799803c009517064d5f6af1462f2a9952
Yaroslav Shukharev <krkmetal@gmail.com>
2016-03-05 14:43:51 +1000

    added question and anwser models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, null=True,
+        on_delete=models.SET_NULL, related_name='questions')
+    likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question, null=True)
+    author = models.ForeignKey(User, null=True,
+        on_delete=models.SET_NULL, related_name='answers')
