9e36727db91b133916dbc68e111e44240d28b9d5
Yaroslav Shukharev <krkmetal@gmail.com>
2016-03-06 03:53:03 +1000

    rating default

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True, blank=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True,
         on_delete=models.SET_NULL, related_name='questions')
     likes = models.ManyToManyField(User)
