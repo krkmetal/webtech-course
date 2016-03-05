7430be22b71829eff915278bd1a8800010cedb19
Yaroslav Shukharev <krkmetal@gmail.com>
2016-03-05 15:07:53 +1000

    mysql settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -34,8 +34,11 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'box_django',
+        'USER': 'box',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1'
     }
 }
 
